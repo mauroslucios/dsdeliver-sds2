b48c27f2b7a55c881ec8c31313e66e9ebac2e986
mauroslucios <mauroslucios@gmail.com>
2021-01-13 16:14:18 -0300

    Add footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import './Navbar/index'
 import Navbar from './Navbar/index';
 import Home from'./Home/index';
+import Footer from './Footer/index';
 
 
 function App() {
---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,19 @@
+import './style.css';
+import { ReactComponent as InstagranIcon } from './img/instagram.svg'; 
+import { ReactComponent as YoutubeIcon } from './img/youtube.svg'; 
+import { ReactComponent as LinkedinIcon } from './img/linkedin.svg'; 
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            <h3>App desenvolvido durante a 2º edição do evento Semana DevSuuperior 2</h3>
+            <div className="footer-icons">
+                    <a href="https://www.instagran.com/luciospsilva" target="_new"><InstagranIcon/></a>
+                    <a href="https:www.youtube.com/channel/UCFUC1CjoLr7PTFVMRAbLkiw" target="_new"><YoutubeIcon/></a>
+                    <a href="https://www.linkedin.com/in/mauro-l%C3%BAcio-pereira/" target="_new"><LinkedinIcon/></a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
