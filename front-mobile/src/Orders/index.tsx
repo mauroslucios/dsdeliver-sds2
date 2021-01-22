c87b509d5778aebf2d16f4d37b94ae657b1dbff5
mauroslucios <mauroslucios@gmail.com>
2021-01-22 09:10:22 -0300

    Created loading

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, ScrollView, Alert } from 'react-native';
+import { StyleSheet, ScrollView, Alert, Text, Image } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import fetchOrders from '../api';
 import Header from '../Header';
@@ -9,22 +9,33 @@ import { Order } from '../types';
 
 function Orders() {
   const [orders, setorders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetchOrders()
       .then(response => setorders(response.data))
-      .catch(() => Alert.alert('Houve erro ao buscar os pedidos!'));
+      .catch(() => Alert.alert('Houve erro ao buscar os pedidos!'))
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
     <>
       <Header />
       <ScrollView style={styles.container}>
-        {orders.map(order => (
-          <TouchableWithoutFeedback key={order.id}>
-            <OrderCard order={order}/>
-          </TouchableWithoutFeedback>
-        ))}
+        {isLoading ? (
+          <>
+          
+          <Image style={styles.loadingImage} source={require('../assets/loading.gif')}/>
+          <Text style={styles.loadingText}>Carregando pedidos...</Text>
+          </>
+          ) : (
+            orders.map(order => (
+              <TouchableWithoutFeedback key={order.id}>
+                <OrderCard order={order} />
+              </TouchableWithoutFeedback>
+            ))
+          )}
       </ScrollView>
     </>
   );
@@ -35,6 +46,18 @@ const styles = StyleSheet.create({
     paddingRight: '5%',
     paddingLeft: '5%',
 
+  },
+  loadingImage: {
+    marginTop: '50%',
+    marginLeft:'25%'
+  },
+  loadingText:{
+    color: '#263238',
+    fontSize: 20,
+    lineHeight: 35,
+    fontWeight: 'normal',
+    marginTop:'-15%',
+    marginLeft:'25%'
   }
 });
 export default Orders;
